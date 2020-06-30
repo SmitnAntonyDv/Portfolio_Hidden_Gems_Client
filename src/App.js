fc7b81380689f8e96715a250191c4d02dd553130
SmitnAntonyDv <anton.smit@hotmail.com>
2020-06-30 14:42:30 +0200

    added the other routing paths

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Homepage from './pages/Homepage';
+
 import { Switch, Route } from 'react-router-dom';
 import Other from './pages/Other';
-
+import Homepage from './pages/Homepage';
+import CountryPage from './pages/CountryPage';
+import DashboardPage from './pages/DashboardPage';
+import DetailsPage from './pages/DetailsPage';
 function App() {
   return (
     <div className='App'>
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/testing' component={Other} />
+        <Route exact path='/locations' component={CountryPage} />
+        <Route path='/locations/:id' component={DetailsPage} />
+        <Route path='/user/:id/dashboard' component={DashboardPage} />
       </Switch>
     </div>
   );
