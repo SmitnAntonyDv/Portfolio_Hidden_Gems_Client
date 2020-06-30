318b1c4de1200db18dbf8d35326457aad6efbd08
SmitnAntonyDv <anton.smit@hotmail.com>
2020-06-30 13:08:05 +0200

    initialize project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>skeleton structure</h1>
+    </div>
+  );
+}
+
+export default App;
