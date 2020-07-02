e12f6e88a2734930e8bd5dd4e040c139edc9c590
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-02 23:22:35 +0200

    styled navbar and homepage buttons - used final color palate

--- src/pages/CountryPage.js
+++ src/pages/CountryPage.js
@@ -2,10 +2,18 @@ import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCountryPosts } from "../store/countrypage/actions";
-
 import { selectSpecificCountryInfo } from "../store/countrypage/selectors";
-
 import CountryAPIcard from "../components/CountryAPIcard";
+
+import {
+  Container,
+  Row,
+  Col,
+  CardDeck,
+  Card,
+  CardGroup,
+  Button,
+} from "react-bootstrap";
 
 export default function CountryPage() {
   const dispatch = useDispatch();
@@ -19,39 +27,48 @@ export default function CountryPage() {
       return <h2>Loading posts chief! . . . </h2>;
     } else {
       return (
-        <div>
-          <div>
-            <button>Most liked</button>
-            <button>Most Recent</button>
-            <button>closest to your location</button>
-          </div>
-          {countryInfo.locationposts.map((post) => {
-            return (
-              <li key={post.id}>
-                <h2>{post.title}</h2>
-                <p>{post.description}</p>
-                <img src={post.imageUrl} alt='' />
-                <p>location :{post.adress}</p>
-                <h2>
-                  TODO: LINK LAT / LONG VALUES to geocoding google API
-                  (according to googleAPI)
-                </h2>
-                <p>
-                  latitude: {post.latitude},
-                  <br />
-                  longitude: {post.longitude}
-                </p>
-                lat:
-                <Link to={`/locations/${post.id}/details`}>
-                  <button>Explore this location!</button>
-                </Link>
-              </li>
-            );
-          })}
-        </div>
+        <Row>
+          <Col>
+            <div>
+              <button>Most liked</button>
+              <button>Most Recent</button>
+              <button>closest to your location</button>
+            </div>
+
+            <CardGroup style={{ flexDirection: "column" }}>
+              {countryInfo.locationposts.map((post) => {
+                return (
+                  <Card key={post.id}>
+                    <Card.Body>
+                      <Card.Title>{post.title}</Card.Title>
+                      <Card.Text>{post.description}</Card.Text>
+                      <Card.Img size='lg' src={post.imageUrl} alt='' />
+                      <Card.Text>location :{post.adress}</Card.Text>
+                      <h2>
+                        TODO: LINK LAT / LONG VALUES to geocoding google API
+                        (according to googleAPI)
+                      </h2>
+                      <p>
+                        latitude: {post.latitude},
+                        <br />
+                        longitude: {post.longitude}
+                      </p>
+                      <Button varient='primary' size='lg'>
+                        <Link to={`/locations/${post.id}/details`}>
+                          Explore this location!
+                        </Link>
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                );
+              })}
+            </CardGroup>
+          </Col>
+        </Row>
       );
     }
   }
+
   function renderCountryInfo() {
     if (!countryInfo.name) {
       // console.log("No info yet chief");
@@ -71,13 +88,10 @@ export default function CountryPage() {
 
   return (
     <div>
-      <p>COUNTRY PAGE</p>
-      <p>To do:</p>
-      <ul>
-        <li>style page according to wireframe</li>
-      </ul>
-      {renderCountryPost()}
-      {renderCountryInfo()}
+      <Container>
+        {renderCountryPost()}
+        {renderCountryInfo()}
+      </Container>
     </div>
   );
 }
