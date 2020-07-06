f04c42fbc02fcdc3fd86dc0dec32124625cdfac0
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-06 17:43:38 +0200

    ADDED: appState reducer

--- src/store/appState/reducer.js
+++ src/store/appState/reducer.js
@@ -1,9 +1,29 @@
-const initialState = {};
+import {
+  APP_LOADING,
+  APP_DONE_LOADING,
+  SET_MESSAGE,
+  CLEAR_MESSAGE,
+} from "./actions";
+import { bindActionCreators } from "redux";
+
+const initialState = {
+  loading: false,
+  message: null,
+};
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
-    // case typeName:
-    //     return { ...state, ...payload }
+    case APP_LOADING:
+      return { ...state, loading: true };
+
+    case APP_DONE_LOADING:
+      return { ...state, loading: false };
+
+    case SET_MESSAGE:
+      return { ...state, message: payload };
+
+    case CLEAR_MESSAGE:
+      return { ...state, message: null };
 
     default:
       return state;
