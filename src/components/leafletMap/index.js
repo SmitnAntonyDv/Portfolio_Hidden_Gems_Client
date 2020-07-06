ee322209bcea3217668dd9b68f5453b75857498f
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-06 12:22:24 +0200

    UPDATE: map completely dynamic | it has a custom marker | onclick it shows PopUp with location adress

--- src/components/leafletMap/index.js
+++ src/components/leafletMap/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import L from "leaflet";
-import { Map, TileLayer, Marker, Popup, Icon } from "react-leaflet";
+import React, { useState } from "react";
+import L, { Icon } from "leaflet";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 delete L.Icon.Default.prototype._getIconUrl;
@@ -11,22 +11,43 @@ L.Icon.Default.mergeOptions({
   shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
 });
 
+const backpackIcon = new Icon({
+  iconUrl: "https://static.thenounproject.com/png/876233-200.png",
+  iconSize: [25, 25],
+});
+
 export default function Mymap(props) {
   console.log("WHAT ARE MY PROPS", props);
   const { id, latitude, longitude, adress } = props;
+  const [togglePopup, setTogglePopup] = useState(false);
+
   return (
     //hardcoded ubud lat and long
-    <Map center={[-8.506854, 115.262482]} zoom={12}>
-      <TileLayer
-        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      />
-      {props ? (
-        <Marker key={id} position={[-8.506854, 115.262482]} />
+    <div>
+      {props.id ? (
+        <Map center={[latitude, longitude]} zoom={12}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          />
+          <Marker
+            key={id}
+            position={[latitude, longitude]}
+            onclick={() => setTogglePopup(!togglePopup)}
+            icon={backpackIcon}
+          />
+          {togglePopup ? (
+            <Popup position={[latitude, longitude]}>
+              {" "}
+              <p>{adress}</p>
+            </Popup>
+          ) : (
+            []
+          )}
+        </Map>
       ) : (
-        <div>Loading map. . .</div>
+        <div>Loading map . . .</div>
       )}
-      {/* <Marker key={id} /> */}
-    </Map>
+    </div>
   );
 }
