442c17330e280183002cc30ed1c057f7f73df9d7
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-07 22:28:53 +0200

    updated footer styling - somehow still have a couple pixels space

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,51 +1,77 @@
 import React from "react";
+import {
+  FaFacebook,
+  FaGooglePlusG,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
 
 export default function Footer() {
   return (
     <div className='main-footer'>
-      <div className='container'>
-        <div className='row'>
-          {/* Colum1 */}
-          <div className='col'>
-            <h4>About Us</h4>
-            <ul className='list-unstyled'>
-              <li>Head Office South-East Asia</li>
-              <li>+65-992-5309-5</li>
-              <li>Singapore</li>
-              <li>Ang mo kio street 82-2</li>
-              <li>Head Office Europe.</li>
-              <li>+44-782-5031-660</li>
-              <li>London, United Kingdom</li>
-              <li>10 Downing Street</li>
-            </ul>
-          </div>
-          {/* colum2 */}
-          <div className='col'>
-            <h4>Join Us</h4>
-            <ul className='list-unstyled'>
-              <li>Discover your future with us</li>
-              <li>Management</li>
-              <li>Company Trips</li>
-            </ul>
-          </div>
-          {/* colum3 */}
-          <div className='col'>
-            <h4>Connect with us</h4>
-            <ul className='list-unstyled'>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
+      <div className='row'>
+        {/* Colum1 */}
+        <div className='col-xs-12 col-sm-6 col-md-3'>
+          <h4 className='footer-header'>About Us</h4>
+          <ul className='list-unstyled'>
+            <li>Head Office South-East Asia</li>
+            <li>+65-992-5309-5</li>
+            <li>Singapore</li>
+            <li>Ang mo kio street 82-2</li>
+          </ul>
+        </div>
+        {/* colum2 */}
+        <div className='col-xs-12 col-sm-6 col-md-3'>
+          <h4 className='footer-header'>Join Us</h4>
+          <ul className='list-unstyled'>
+            <li>
+              <a className='fake-Link' href='#' onClick='return false;'>
+                Explore positions
+              </a>
+            </li>
+            <li className='fake-Link'>Management</li>
+            <li className='fake-Link'>Company Trip</li>
+          </ul>
+        </div>
+        {/* colum3 */}
+        <div className='col-xs-12 col-sm-6 col-md-3'>
+          <h4 className='footer-header'>About Us</h4>
+          <ul className='list-unstyled'>
+            <li>Head Office South-East Asia</li>
+            <li>+65-992-5309-5</li>
+            <li>Singapore</li>
+            <li>Ang mo kio street 82-2</li>
+          </ul>
+        </div>
+        {/* colum4 */}
+        <div className='col-xs-12 col-sm-6 col-md-3'>
+          <h4 className='footer-header'>Connect</h4>
+          <div className='footer-social'>
+            <div className='icons-horizontal'>
+              <ul className='list-unstyled'>
+                <li>
+                  <FaFacebook />
+                </li>
+                <li>
+                  <FaGooglePlusG />
+                </li>
+                <li>
+                  <FaInstagram />
+                </li>
+                <li>
+                  <FaTwitter />
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-        <hr />
-        <div className='row'>
-          <p className='col-sm'>
-            &copy;{new Date().getFullYear} Hidden Gems Inc | all rights reserved
-            | Terms of Service | Privacy
-          </p>
-        </div>
+      </div>
+      <hr />
+      <div className='row'>
+        <p className='col-sm'>
+          &copy;2020 Hidden Gems Inc | all rights reserved | Terms of use |
+          Privacy & cookies
+        </p>
       </div>
     </div>
   );
