588e71443d214b95185c39ec4001c38a3af1e0ce
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-08 21:42:13 +0200

    added current location Marker functionality

--- src/components/leafletMap/index.js
+++ src/components/leafletMap/index.js
@@ -18,15 +18,28 @@ const backpackIcon = new Icon({
   iconUrl: "https://static.thenounproject.com/png/876233-200.png",
   iconSize: [25, 25],
 });
+const gemIcon = new Icon({
+  iconUrl:
+    "https://toppng.com/uploads/preview/em-svg-png-icon-free-download-gem-icon-11563228146u2haxp4svc.png",
+  iconSize: [25, 25],
+});
+const hifigure = new Icon({
+  iconUrl:
+    "https://spng.pinpng.com/pngs/s/457-4574663_png-file-svg-happy-person-icon-png-transparent.png",
+  iconSize: [25, 25],
+});
 
 export default function Mymap(props) {
   const { id, latitude, longitude, adress } = props;
   const User = useSelector(selectUser);
   const [togglePopup, setTogglePopup] = useState(false);
   const [toggleTracking, setToggleTracking] = useState(false);
+  const [toggleCurrentLoc, setToggleCurrentLoc] = useState(false);
   const [trackingBtnText, setTrackingBtnText] = useState(false);
   const [updatedLatitude, setUpdatedLatitude] = useState("");
   const [updatedLongitude, setUpdatedLongitude] = useState("");
+  const [lat, setLat] = useState("");
+  const [lon, setLon] = useState("");
 
   // user coords switch block
   let userLocation;
@@ -55,6 +68,7 @@ export default function Mymap(props) {
   }
   const options = { enableHighAccuracy: true };
   watchUserLocation();
+  getCurrentLoc();
 
   //toggle function
   function toggleUserTracking() {
@@ -67,11 +81,32 @@ export default function Mymap(props) {
   } else {
     trackOrNot = "Enable Tracking";
   }
+  function toggleUserCurrentLoc() {
+    setToggleCurrentLoc(!toggleCurrentLoc);
+  }
 
+  // current location marker
+  function CurrentUserLoc(pos) {
+    setLat(pos.coords.latitude);
+    setLon(pos.coords.longitude);
+  }
+  function getCurrentLoc() {
+    return navigator.geolocation.getCurrentPosition(
+      CurrentUserLoc,
+      error,
+      options
+    );
+  }
+
+  console.log("CURRENT LAT", lat);
+  console.log("CURRENT LON", lon);
   return (
     <div>
       <Button className='toggleTrack' onClick={toggleUserTracking}>
         {trackOrNot}
+      </Button>
+      <Button className='toggleTrack' onClick={toggleUserCurrentLoc}>
+        Show Current Position
       </Button>
       {props.id ? (
         <Map center={[latitude, longitude]} zoom={12}>
@@ -83,7 +118,7 @@ export default function Mymap(props) {
             key={id}
             position={[latitude, longitude]}
             onclick={() => setTogglePopup(!togglePopup)}
-            icon={backpackIcon}
+            icon={gemIcon}
           />
           {togglePopup ? (
             <Popup position={[latitude, longitude]}>
@@ -100,6 +135,11 @@ export default function Mymap(props) {
               position={[userLocation.lat, userLocation.lon]}
               icon={backpackIcon}
             />
+          ) : (
+            <></>
+          )}
+          {!!lat && !!lon && !!toggleCurrentLoc ? (
+            <Marker key={95103} position={[lat, lon]} icon={hifigure} />
           ) : (
             <></>
           )}
