1801daf55a2749455b71f997defaa1dcdf869d93
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-10 14:13:51 +0200

    aptly name log

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,4 @@
+console.log(" WHAT THE FCK IS THIS ?!", process.env)
 export const url = process.env.REACT_APP_API_URL || `http://localhost:4000`;
 export const geocodeAPI = `https://api.opencagedata.com/geocode/v1/json?`;
 export const geoS = "59e64031284240d8bbc808ccca9f1b47";
