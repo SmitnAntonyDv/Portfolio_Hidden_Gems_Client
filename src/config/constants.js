92f95ee55d7a941bcc9228383c960a46a5e86cdd
SmitnAntonyDv <anton.smit@hotmail.com>
2020-07-23 19:59:09 +0200

    hotfix url DB

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
-export const url = `http://explore-hidden-gems.herokuapp.com`;
+export const url = `https://explore-hidden-gems.herokuapp.com/`;
 export const geocodeAPI = `https://api.opencagedata.com/geocode/v1/json?`;
 export const geoS = "59e64031284240d8bbc808ccca9f1b47";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
